6ad6d639347af25d391ede78004222de113448e5
Alizée <alizee.soussan@epitech.eu>
2022-02-01 15:13:48 +0100

    [+] test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
